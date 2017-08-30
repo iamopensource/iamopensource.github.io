3f2f768e3dd9069381ae21ca76fce5dcb8928047
anglinb <anglinb@github.com>
2017-08-29 18:03:29 -0700

    Adds deployment script

--- server/deploy.sh
+++ server/deploy.sh
@@ -14,18 +14,31 @@ echo "Deployment processing at --> $NOW_URL"
 echo "Beginning smoke test..."
 ATTEMPTS=0
 MAX_ATTEMPTS=10
-while [ $ATTEMPTS -lt $MAX_ATTEMPTS] 
+while [ $ATTEMPTS -lt $MAX_ATTEMPTS]
 do
   echo "Pausing for 2 seconds..."
   sleep 2
   RESPONSE_STATUS_CODE=$(curl --write-out %{http_code} --silent --output /dev/null $NOW_URL/api/contriubtions)
   if [ $RESPONSE_STATUS_CODE == "200" ] then
     echo "Smoke test passed!, updating pages site to point to new deployment..."
-    cd ..
+    rm -rf tmp
+    git clone git@github.com:iamopensource/iamopensource.github.io.git tmp
+    cd tmp
     PREPARED_NOW_URL=$(echo "$NOW_URL" |  sed -E 's/\//\\\//g' | sed -E 's/\./\\./g')
-    sed -i '' -E 's/https:\/\/iamopensource-[a-z]+\.now\.sh/$PREPARED_NOW_URL/g' index.html
-    git commit -m "Updates the backend url $NOW_URL"
-    echo "Commit added run git push origin master to update the front end"
+    sed -i '' -E 's/https:\/\/iamopensource-[a-z]+\.now\.sh/'"${PREPARED_NOW_URL}"'/g' index.html
+    git add index.html
+    git commit -m "Updates the backend url to $NOW_URL"
+    echo "Commit created..."
+    while true; do
+        read -p "Do you want deploy the front end?" yn
+        case $yn in
+            [Yy]* ) git push origin master; break;;
+            [Nn]* ) exit;;
+            * ) echo "Please answer yes or no.";;
+        esac
+    done
+
+    echo "Deployment complete! Please spot check https://iamopensource.github.io looks good!"
   else
     echo "Smoke test failed with error code $RESPONSE_STATUS_CODE, retrying..."
   fi
